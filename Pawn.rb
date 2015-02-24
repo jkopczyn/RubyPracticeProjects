a81042e9957ae31a30f29c8c2d9e2dc68b441dff
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-02-24 14:44:40 -0800

    Finish Pawn moves

--- Pawn.rb
+++ Pawn.rb
@@ -13,14 +13,36 @@ class Pawn < Piece
   end
 
   def moves
+    possible_spaces = []
+    if @color == :white
+      diagonal_spaces = DIAGONALS_UP.map {|diff| stepped_position(diff) }
+      forward_one = stepped_position(UP)
+      forward_two = stepped_position(UP_TWO)
+    elsif @color == :black
+      diagonal_spaces = DIAGONALS_DOWN.map {|diff| stepped_position(diff) }
+      forward_one = stepped_position(DOWN)
+      forward_two = stepped_position(DOWN_TWO)
+    else
+      raise TypeError.new("We do not support 50 shades of grey")
+    end
 
+    diagonal_spaces.each do |space|
+      if @board.occupied?(space) && @board.whats_here(space).color == ( @color== :white ? :black : :white )
+        possible_spaces << space
+      end
+    end
 
+    if !@board.occupied?(forward_one) &&
+      possible_spaces << forward_one
+      if !@has_moved? && !@board.occupied?(forward_two)
+        possible_spaces << forward_two
+      end
+    end
+  end
 
-
-    #hasn't moved yet (2 spaces)
-    #pieces to kill
-    #obstruction in front
-
+  def stepped_position(step)
+    [@position[0]+step[0],
+     @position[1]+step[1]]
   end
 
 end
